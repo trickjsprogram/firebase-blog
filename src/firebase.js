1f8f9860bead0b7a2261f6e96f49e4f11d406309
trickjsprogram <79596631+trickjsprogram@users.noreply.github.com>
2022-05-29 01:15:35 +0530

    Update firebase file

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCW_lL5bpXEickNp1PD1Nyq3-BW6LZQcZ0",
-  authDomain: "react-blogs-app.firebaseapp.com",
-  projectId: "react-blogs-app",
-  storageBucket: "react-blogs-app.appspot.com",
-  messagingSenderId: "520626893881",
-  appId: "1:520626893881:web:5432b7c30440ec4a0749e8",
+  apiKey: "apikey",
+  authDomain: "test",
+  projectId: "test",
+  storageBucket: "test",
+  messagingSenderId: "your id",
+  appId: "appid",
 };
 
 const app = initializeApp(firebaseConfig);
